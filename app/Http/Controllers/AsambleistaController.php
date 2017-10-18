22f77a9ac1e9b97f2562fab90ecf22f07a7edb1c
Jonatan <mcr.jblh@gmail.com>
2017-10-17 23:10:55 -0600

    Funcionalidad de presentar asambleista por comision

--- app/Http/Controllers/AsambleistaController.php
+++ app/Http/Controllers/AsambleistaController.php
@@ -17,11 +17,23 @@ class AsambleistaController extends Controller
     public function asambleistas_comision(){
 
         //recuperar las comisiones que estan activas en el periodo vigente
-        $comisiones = Comision::where("activa","=",1)->get();
-        //se obtienen todos los asambleistas
-        //$asambleistas = DB::table('cargos')->join('asambleistas','cargos.asambleista_id','=','asambleistas.id')->where('asambleistas.activo','=',1)->get();
-        $asambleistas = Cargo::all();
+        $comisiones = Comision::where("activa",1)->get();
 
-        return view("Asambleistas.ListadoAsambleistasComision",['comisiones'=>$comisiones, 'asambleistas'=>$asambleistas]);
+        //Se genera un array con el id de las comisiones activas
+        //$idComisiones  = Comision::where('activa' ,'=' ,1)->pluck('id')->toArray();
+
+        //Se obtiene todos los cargos que pertenecen a una comision activa y que sea un asambleista activo
+        //$cargos = Cargo::whereIn("comision_id",$idComisiones)->join("asambleistas","cargos.asambleista_id","=","asambleistas.id")->where("asambleistas.activo","=",1)->get();
+
+        //se obtiene todos los asambleistas que pertenecen a una comision activa y que estos esten
+        //activos
+        $cargos = Cargo::join("asambleistas","cargos.asambleista_id","=","asambleistas.id")
+            ->join("comisiones","cargos.comision_id","=","comisiones.id")
+            ->where("asambleistas.activo","=",1)
+            ->where("comisiones.activa","=",1)
+            ->get();
+
+        return view("Asambleistas.ListadoAsambleistasComision",['comisiones'=>$comisiones, 'cargos'=>$cargos]);
+
     }
 }
--- resources/views/Asambleistas/ListadoAsambleistasComision.blade.php
+++ resources/views/Asambleistas/ListadoAsambleistasComision.blade.php
@@ -5,14 +5,47 @@
 @endsection
 
 @section("content")
-    <div>
-        @foreach($comisiones as $comision)
-            <p>{{ $comision->nombre }}</p>
-        @endforeach
-
-            @foreach($asambleistas as $asambleista)
-                {{ $asambleista->cargo }}
+    <div class="box box-default box-solid">
+        <div class="box-header with-border">
+            <h3 class="box-title">Listado de Asambleistas por Comision</h3>
+        </div>
+        <div class="box-body">
+            @foreach($comisiones as $comision)
+                <div class="panel panel-default ">
+                    <div class="panel-heading text-bold text-capitalize">{{ $comision->nombre }}</div>
+                    <div class="table-responsive">
+                        <table class="table text-center">
+                            <thead>
+                            <tr>
+                                <th>Numero</th>
+                                <th>Imagen</th>
+                                <th>Asambleista</th>
+                                <th>Sector</th>
+                                <th>Cargo</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @php $i = 1 @endphp
+                            @foreach($cargos as $cargo)
+                                @if($comision->id == $cargo->comision->id)
+                                    <tr>
+                                        <td>{{ $i }}</td>
+                                        <td></td>
+                                        <td>{{ $cargo->asambleista->user->persona->primer_nombre . " " . $cargo->asambleista->user->persona->segundo_nombre . " " . $cargo->asambleista->user->persona->primer_apellido . " " . $cargo->asambleista->user->persona->segundo_apellido }}</td>
+                                        <td>{{ $cargo->asambleista->sector->nombre }}</td>
+                                        <td>{{ $cargo->cargo }}</td>
+                                    </tr>
+                                @endif
+                                @php $i++ @endphp
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             @endforeach
+
+            @php $i = 0 @endphp
+        </div>
     </div>
 @endsection
 
